8415a0ad65e13e83d814290d98e34bc65450522a
Abdukhaligov <hikmat.pou@gmail.com>
2024-02-01 18:51:15 +0400

    update: overwrite `WipeCommand` command

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('/authenticate', [AuthController::class, 'authenticate']);
 
 Route::middleware('auth')->group(function () {
