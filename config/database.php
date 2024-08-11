985375418603ad5ea32f1d0bf48ae365446954c3
Abdukhaligov <hikmat.pou@gmail.com>
2024-08-11 21:48:27 +0400

    update: db_log -> db_shared, move config to shared package

--- config/database.php
+++ config/database.php
@@ -92,16 +92,6 @@ return [
             // 'encrypt' => env('DB_AUTH_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_AUTH_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
-
-        'db_log' => [
-            'driver' => env('DB_LOG_CONNECTION', 'mysql'),
-            'host' => env('DB_LOG_HOST', '127.0.0.1'),
-            'port' => env('DB_LOG_PORT', '3306'),
-            'database' => env('DB_LOG_DATABASE', 'phober_log'),
-            'username' => env('DB_LOG_USERNAME', 'forge'),
-            'password' => env('DB_LOG_PASSWORD', ''),
-        ],
-
     ],
 
     /*
