beddf6b2aed1909f6edb9f4e98910e190f6c88ee
Hikmat Abdukhaligov <hikmat.abdukhaligov@americor.com>
2026-07-19 02:30:39 +0400

    add: AuthUser model p.2

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -4,15 +4,36 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Phobiavr\PhoberLaravelCommon\Contracts\AuthUserInterface;
 
-class UserResource extends JsonResource {
+class UserResource extends JsonResource implements AuthUserInterface {
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function getUsername(): string {
+        return $this->username;
+    }
+
+    public function getFirstName(): string {
+        return $this->first_name;
+    }
+
+    public function getLastName(): string {
+        return $this->last_name;
+    }
+
+    public function getEmail(): string {
+        return $this->email;
+    }
+
     public function toArray(Request $request): array {
         return [
-            'id'         => $this->id,
-            'username'   => $this->username,
-            'first_name' => $this->first_name,
-            'last_name'  => $this->last_name,
-            'email'      => $this->email,
+            self::FIELD_ID         => $this->getId(),
+            self::FIELD_USERNAME   => $this->getUsername(),
+            self::FIELD_FIRST_NAME => $this->getFirstName(),
+            self::FIELD_LAST_NAME  => $this->getLastName(),
+            self::FIELD_EMAIL      => $this->getEmail(),
         ];
     }
 }
